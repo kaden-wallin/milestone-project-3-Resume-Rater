8fdfaa73bbd5b0ecbb4b72d20191464dd61c27cc
kaden-wallin <109696739+kaden-wallin@users.noreply.github.com>
2023-05-01 16:45:13 -0600

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://rotten-resumes-backend.azurewebsites.net',
+            target: 'https://localhost:5000',
             changeOrigin: true,
         })
     );
